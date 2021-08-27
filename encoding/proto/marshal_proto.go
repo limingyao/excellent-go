7f660d19a6ebdc9efbfdfe38060c0abe66f5a2db
limingyao <limingyao668@vip.qq.com>
2021-08-27 23:33:01 +0800

    feat(http): add gee

--- encoding/proto/marshal_proto.go
+++ encoding/proto/marshal_proto.go
@@ -5,6 +5,7 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 )
 
+// ProtoMarshaller is a Marshaller which marshals/unmarshals into/from serialize proto bytes
 type ProtoMarshaller struct {
 	runtime.ProtoMarshaller
 }
