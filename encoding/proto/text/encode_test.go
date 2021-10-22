90c4520da88120b23fd5ff67f65a45f46d118d9e
limingyao <limingyao668@vip.qq.com>
2021-10-22 23:36:39 +0800

    test(proto): update test

--- encoding/proto/text/encode_test.go
+++ encoding/proto/text/encode_test.go
@@ -3,16 +3,17 @@ package text
 import (
 	"github.com/golang/protobuf/proto"
 	"github.com/google/uuid"
+	"github.com/limingyao/excellent-go/test"
 	"testing"
 )
 
 func TestReadable(t *testing.T) {
-	hello := &Hello{}
+	hello := &test.Hello{}
 	hello.SessionId = uuid.New().String()
 	hello.InstanceId = 10000
 	hello.Names = append(hello.Names, "hello", "world")
 	hello.Version = "v1.0"
-	hello.Data = &Data{}
+	hello.Data = &test.Data{}
 	hello.Data.Video = []byte("video")
 	hello.Data.Images = make(map[int32][]byte)
 	hello.Data.Images[0] = []byte("first image")
