ab937a31fc3d3837225e2a7b4e272335ab866634
limingyao <limingyao668@vip.qq.com>
2021-10-23 23:38:51 +0800

    refactor(test): update generate.sh

--- test/generate.sh
+++ test/generate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+WORK_DIR="$(cd "$(dirname "$0")"/.. || exit 1; pwd)"
+cd "$WORK_DIR" || exit 1
+
 protoc -I . --go_out=paths=source_relative:. test/test.proto
 
 protoc -I . -I ../googleapis --go_out . --go_opt paths=source_relative \
